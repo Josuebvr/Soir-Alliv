2363cda3aff8a34af579d767e2925f80ef3debf8
Josuebvr <josuebvrios@gmail.com>
2025-12-17 11:32:57 -0300

    Um pequeno teste...

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js - GUARDE ESTE TOKEN COM SEGURANÇA!
+const GITHUB_CONFIG = {
+    owner: 'Josuebvr',
+    repo: 'soir-comments',
+    token: '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', // seu token gerado acima
+    branch: 'main'
+};
