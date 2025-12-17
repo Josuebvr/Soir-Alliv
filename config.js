2fec31381e94068e183583ba7f54d0fd2d874de1
Josuebvr <josuebvrios@gmail.com>
2025-12-17 15:20:30 -0300

    Voltando...

--- config.js
+++
@@ -1,7 +0,0 @@
-// config.js - GUARDE ESTE TOKEN COM SEGURANÇA!
-const GITHUB_CONFIG = {
-    owner: 'Josuebvr',
-    repo: 'soir-comments',
-    token: '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', // seu token gerado acima
-    branch: 'main'
-};
